6fd359eafbaad0db3a0496db22df8c5cc9c88a08
Dostonbek1 <toirovd@berea.edu>
2018-07-13 02:07:58 -0400

    Just pushing the content

---
+++ README.md
@@ -0,0 +1,3 @@
+# Personal Website - <a href="http://dostonbek.com/" target="_blank">www.dostonbek.com</a>
+ 
+This is my personal profile website. The content might not be the same as in the actual website, and that is because I use a different web hosting for the website.
---
+++ js/typing.js
@@ -0,0 +1,50 @@
+var temp = 1;
+
+(function runForever(){
+  test();
+  temp += 1
+  setTimeout(runForever, 5500)
+})()
+
+function test() {
+  if (temp % 3 == 1) {
+    var txt = 'Hello! Welcome to My Website!';
+  } else if (temp % 3 == 2) {
+    var txt = 'My name is Dostonbek';
+  } else {
+    var txt = 'I am a Developer';
+  }
+  var i = 0;
+  var speed = 80;
+  var demo = document.getElementById("demo");
+  var text_list = '';
+  var j = txt.length;
+
+  typeWriter(i, txt, demo, text_list, j);
+}
+
+function typeWriter(i, txt, demo, text_list, j) {
+  text_list += txt.charAt(i);
+  if (i < txt.length) {
+
+    demo.innerHTML = text_list;
+    i++;
+    setTimeout(function(){typeWriter(i, txt, demo, text_list, j)}, 110);
+  } else {
+    erase(text_list, demo, j);
+  }
+
+function erase(text_list, demo, j) {
+  if (j != 0) {
+
+    text_list = text_list.slice(0, -1)
+    demo.innerHTML = text_list;
+    j = j - 1;
+    console.log(j);
+    setTimeout(function(){ erase(text_list, demo, j ) }, 60);
+    console.log(j);
+  }
+}
+
+
+}
